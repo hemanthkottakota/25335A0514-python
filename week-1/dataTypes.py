eba7617f7bb116a2081784239066b0ee6f18f370
hemanth <hemanthkottakota07@gmail.com>
2025-12-13 21:49:08 +0530

    updated week-1 and week-2 programs

---
+++ week-1/dataTypes.py
@@ -0,0 +1,12 @@
+intVar = 10
+stringVar = "hemanth"
+floatVar = 3.14
+boolVar = True
+listVar = [1,2,3,"hello"]
+tupleVar = (1,"hi",3)
+print(type(intVar))
+print(type(stringVar))
+print(type(floatVar))
+print(type(boolVar))
+print(type(listVar))
+print(type(tupleVar))
