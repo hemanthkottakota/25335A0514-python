7fcf25c225aea3808e90abe0803802f737cd12cf
hemanthkottakota <hemanthkottakota07@gmail.com>
2026-01-07 22:41:13 +0530

    factorial recursion and non recursion

---
+++ week-5/factorial.py
@@ -0,0 +1,27 @@
+
+def factorial(n):
+    fact = 1
+    # if n == 0 or n == 1:
+    #     return 1
+    for i in range(1,n+1):
+        fact*=i
+    return fact
+    
+def factorial_recursion(n):
+    if n == 0 or n == 1:
+        return 1
+    return n*factorial_recursion(n-1)
+
+
+
+while True:
+    choice = int(input("enter 1 - recurrsion \nenter 2 - non recurssion\n 3 - exit"))
+    if choice == 1:
+        num = int(input("enter a number"))
+        value = factorial_recursion(num)
+    elif choice == 2:
+        num = int(input("enter a number"))
+        value = factorial(num)
+    else:
+        break
+    print(f'"factorial of {num} : {value}')
