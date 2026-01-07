5c9691c8a5b2614ffbc7375a78a34fd3fc042ecf
hemanthkottakota <hemanthkottakota07@gmail.com>
2026-01-07 22:20:42 +0530

    prime recursion and non recursion

---
+++ week-5/prime.py
@@ -0,0 +1,34 @@
+import math
+def prime(num):
+    for i in range(2,int(math.sqrt(num//2))+1):
+        if num%i == 0:
+            print(f"{num} is not a prime number")
+            return
+    print(f"{num} is prime number")
+
+
+
+
+
+
+def recurPython(num,i = 2):
+    count = 0
+    if i>math.sqrt(num):
+        return True
+    if num % i == 0:
+        return False
+    else:
+        return recurPython(num,i+1)
+
+
+while True:
+    choice = int(input("enter 1 - recurrsion \nenter 2 - non recurssion\n 3 - exit"))
+    if choice == 1:
+        num = int(input("enter a number"))
+        call = recurPython(num)
+        print(f"{num} is prime\n") if call !=0 else print(f"{num} is not a prime\n")
+    elif choice == 2:
+        num = int(input("enter a number"))
+        prime(num)
+    else:
+        break
